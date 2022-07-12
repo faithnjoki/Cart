dd93e83b18cd7324ed5322c3b312a1a1d8d54e25
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-07-12 15:42:01 +0300

    counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Counter  extends Component {
+    state = { 
+
+     };
+    render() { 
+        return (
+        <React.Fragment>
+        
+            <h1>Hello world</h1>
+            <hr />
+            <button class="btn btn-primary">Add</button>
+        </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter ;
