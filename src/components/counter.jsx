6975b39e352a11442d9bae6a41d1202a2506a492
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-07-12 16:00:14 +0300

    add expression

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 
 class Counter  extends Component {
     state = { 
+        count: 0
 
      };
     render() { 
         return (
+            // use react fragment to avoid  too  many divs 
         <React.Fragment>
-        
-            <h1>Hello world</h1>
+            <h1>Hello React Js</h1>
             <hr />
+           
+            <span>{ this.countexpresion()}</span>
+           
+            <br />
             <button class="btn btn-primary">Add</button>
+           
         </React.Fragment>
         );
+    }
+    countexpresion(){
+        return this.state.count === 0? <h5>Zero</h5>: this.state.count;
     }
 }
  
